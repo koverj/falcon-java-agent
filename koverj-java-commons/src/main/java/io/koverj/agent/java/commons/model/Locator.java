1119f2aea37a96c289c001770da79ca7276d3aa4
Alexey <alpa@ciklum.com>
2020-03-03 11:29:32 +0200

    Feature 6 split project to modules (#10)
    * Add kover server integration
    
    * Move code to main dir for creation jar file
    
    * Rename packages and extract KoverjClient
    
    * #6 Add basic module split

---
+++ koverj-java-commons/src/main/java/io/koverj/agent/java/commons/model/Locator.java
@@ -0,0 +1,56 @@
+package io.koverj.agent.java.commons.model;
+
+/**
+ * Created by alpa on 2/25/20
+ */
+public class Locator {
+
+    private String uuid;
+    private String url;
+    private String subject;
+    private String locator;
+    private String parentUuid;
+
+    public Locator(String uuid, String url, String subject, String locator, String parentUuid) {
+        this.uuid = uuid;
+        this.url = url;
+        this.subject = subject;
+        this.locator = locator;
+        this.parentUuid = parentUuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getLocator() {
+        return locator;
+    }
+
+    public void setLocator(String locator) {
+        this.locator = locator;
+    }
+
+    public String getParentUuid() {
+        return parentUuid;
+    }
+
+    @Override
+    public String toString() {
+        return "Locator{" +
+                "uuid='" + uuid + '\'' +
+                ", url='" + url + '\'' +
+                ", subject='" + subject + '\'' +
+                ", locator='" + locator + '\'' +
+                ", parentUuid='" + parentUuid + '\'' +
+                '}';
+    }
+}
