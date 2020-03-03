0a07f24e8d9d19fe780e742a66c3a2fdc2c2763c
Alexey Panashchenko <alpa@ciklum.com>
2020-03-03 11:13:45 +0200

    #6 Add basic module split

---
+++ koverj-java-commons/src/main/java/io/koverj/agent/java/commons/RetrofitClient.java
@@ -0,0 +1,20 @@
+package io.koverj.agent.java.commons;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by alpa on 2/27/20
+ */
+public class RetrofitClient {
+
+    private static final String KOVERJ_URL = System.getProperty("koverj.url", "http://localhost:8086");
+
+    public static <T> T createService(final Class<T> service) {
+        return  new Retrofit.Builder()
+                .baseUrl(KOVERJ_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(service);
+    }
+}
---
+++ koverj-java-commons/src/main/java/io/koverj/agent/java/commons/model/LocatorResult.java
@@ -0,0 +1,34 @@
+package io.koverj.agent.java.commons.model;
+
+import java.util.List;
+
+/**
+ * Created by alpa on 2/25/20
+ */
+public class LocatorResult {
+
+    private String name;
+    private List<Locator> locators;
+
+
+    public LocatorResult(String name, List<Locator> locators) {
+        this.name = name;
+        this.locators = locators;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Locator> getLocators() {
+        return locators;
+    }
+
+    @Override
+    public String toString() {
+        return "LocatorResult{" +
+                "name=" + name +
+                ", locators=" + locators +
+                '}';
+    }
+}
