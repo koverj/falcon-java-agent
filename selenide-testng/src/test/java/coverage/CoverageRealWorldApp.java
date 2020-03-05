a622e4b44b3e3531b5b0842e5bd688420726afd6
Serhii Pirohov <sergio_89@ukr.net>
2020-03-05 21:23:56 +0200

    Add more ui tests with locators (#18)

--- selenide-testng/src/test/java/coverage/CoverageRealWorldApp.java
+++ selenide-testng/src/test/java/coverage/CoverageRealWorldApp.java
@@ -12,6 +12,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 @Listeners({LocatorTestListener.class})
 public class CoverageRealWorldApp {
@@ -24,43 +25,65 @@ public class CoverageRealWorldApp {
 
     @BeforeMethod
     public void before() {
+        Selenide.open(Configuration.baseUrl);
         SelenideLogger.addListener("LocatorEventsListener", new LocatorEventsListener());
     }
 
     @Test
-    void testSimpleLocators() {
-        Selenide.open(Configuration.baseUrl);
-
-        $("body > app-root > app-layout-header > nav > div > ul > li:nth-child(2) > a").click();
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > fieldset:nth-child(2) > input").setValue("sergio_89@ukr.net");
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > fieldset:nth-child(3) > input").setValue("12345678");
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > button").click();
+    void testSimpleCssLocators() {
+        $("[href='/login']").click();
+        $("[formcontrolname='email']").setValue("sergio_89@ukr.net");
+        $("[formcontrolname='password']").setValue("12345678");
+        $("[type='submit']").click();
     }
 
     @Test
-    void testRelativeLocators() {
-        Selenide.open(Configuration.baseUrl);
+    void testSimpleXpathLocators() {
+        $x("//*[@href='/login']").click();
+        $x("//*[@formcontrolname='email']").setValue("sergio_89@ukr.net");
+        $x("//*[@formcontrolname='password']").setValue("12345678");
+        $x("//button[@type='submit']").click();
+    }
 
+    @Test
+    void testRelativeCssLocators() {
         SelenideElement appRootElement = $("body > app-root");
         SelenideElement navBarElement = appRootElement.$("app-layout-header > nav");
-        SelenideElement link = navBarElement.$("div > ul > li:nth-child(2) > a");
+        SelenideElement link = navBarElement.$("[href='/login']");
         link.click();
     }
 
     @Test
-    void testSimpleAndRelativeLocators() {
-        Selenide.open(Configuration.baseUrl);
+    void testRelativeXpathLocators() {
+        SelenideElement appRootElement = $x("//body//app-root");
+        SelenideElement navBarElement = appRootElement.$x(".//app-layout-header//nav");
+        SelenideElement link = navBarElement.$x(".//*[@href='/login']");
+        link.click();
+    }
 
-        $("body > app-root > app-layout-header > nav > div > ul > li:nth-child(2) > a").click();
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > fieldset:nth-child(2) > input").setValue("sergio_89@ukr.net");
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > fieldset:nth-child(3) > input").setValue("12345678");
-        $("body > app-root > app-auth-page > div > div > div > div > form > fieldset > button").click();
-
-        Selenide.open(Configuration.baseUrl);
+    @Test
+    void testSimpleAndRelativeCssLocators() {
+        $("[href='/login']").click();
+        $("[formcontrolname='email']").setValue("sergio_89@ukr.net");
+        $("[formcontrolname='password']").setValue("12345678");
+        $("[type='submit']").click();
 
         SelenideElement appRootElement = $("body > app-root");
         SelenideElement navBarElement = appRootElement.$("app-layout-header > nav");
-        SelenideElement link = navBarElement.$("div > ul > li:nth-child(2) > a");
+        SelenideElement link = navBarElement.$("[href='/login']");
+        link.click();
+    }
+
+    @Test
+    void testSimpleAndRelativeXpathLocators() {
+        $x("//*[@href='/login']").click();
+        $x("//*[@formcontrolname='email']").setValue("sergio_89@ukr.net");
+        $x("//*[@formcontrolname='password']").setValue("12345678");
+        $x("//button[@type='submit']").click();
+
+        SelenideElement appRootElement = $x("//body//app-root");
+        SelenideElement navBarElement = appRootElement.$x(".//app-layout-header//nav");
+        SelenideElement link = navBarElement.$x(".//*[@href='/login']");
         link.click();
     }
 }
