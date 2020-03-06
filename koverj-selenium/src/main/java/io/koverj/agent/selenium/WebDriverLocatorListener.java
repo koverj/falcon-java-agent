701ec52010fc076bef09117e8d728f918c132be1
Alexey Panashchenko <alpa@ciklum.com>
2020-03-06 13:11:55 +0200

    Add selenium relative locators

--- koverj-selenium/src/main/java/io/koverj/agent/selenium/WebDriverLocatorListener.java
+++ koverj-selenium/src/main/java/io/koverj/agent/selenium/WebDriverLocatorListener.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.LinkedList;
 import java.util.UUID;
 
 public class WebDriverLocatorListener extends LocatorEventListener {
 
+    private static final String HAS_PARENT_ELEMENT = "Has parent element";
     private final LocatorsLifecycle lifecycle;
 
     public WebDriverLocatorListener() {
@@ -25,8 +27,29 @@ public class WebDriverLocatorListener extends LocatorEventListener {
                 .uuid(uuid)
                 .locator(by.toString())
                 .url(currentUrl)
+                .subject(getSubject(element))
                 .build();
 
-        lifecycle.getStorage().put(locator);
+        System.out.println(locator);
+
+        LinkedList<Locator> locators = lifecycle.getStorage().get();
+        if (!locators.isEmpty()) {
+            Locator previousLocator = locators.getLast();
+            if (previousLocator != null) {
+                if (locator.getSubject() != null) {
+                    String parentUuid = previousLocator.getUuid();
+                    locator.setParentUuid(parentUuid);
+                    lifecycle.getStorage().put(locator);
+                } else {
+                    lifecycle.getStorage().put(locator);
+                }
+            }
+        } else {
+            lifecycle.getStorage().put(locator);
+        }
+    }
+
+    private String getSubject(WebElement element) {
+        return element == null ? null : HAS_PARENT_ELEMENT;
     }
 }
--- koverj-selenium/src/test/java/io/koverj/agent/selenium/tests/SeleniumLocatorsTest.java
+++ koverj-selenium/src/test/java/io/koverj/agent/selenium/tests/SeleniumLocatorsTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.koverj.agent.java.commons.LocatorsLifecycle;
 import io.koverj.agent.selenium.LocatorsWatchingDriver;
 import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,24 +15,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SeleniumLocatorsTest {
 
-    @Test
-    void testSeleniumLocators() {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
         System.setProperty("log.locators", "true");
         System.setProperty("use.koverj", "false");
 
         WebDriverManager.chromedriver().setup();
         WebDriver chromeDriver = new ChromeDriver();
+        driver = new LocatorsWatchingDriver(chromeDriver);
+        driver.get("https://angular.realworld.io/");
+    }
 
-        WebDriver driver = new LocatorsWatchingDriver(chromeDriver);
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
 
+    @Test
+    void testSeleniumLocators() {
         driver.get("https://angular.realworld.io/");
 
         driver.findElement(By.cssSelector("body > app-root > app-layout-header > nav > div > ul > li:nth-child(2) > a"));
 
         driver.findElement(By.cssSelector("body > app-root"))
                 .findElement(By.cssSelector("app-layout-header > nav"));
-
-        driver.quit();
 
         assertThat(LocatorsLifecycle.getInstance().getStorage().get().size(),
                 CoreMatchers.equalTo(3));
